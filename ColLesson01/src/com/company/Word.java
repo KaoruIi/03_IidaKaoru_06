86d6ebf4569e82cf40354d1037c5b068f95f2cf3
KaoruIi <xun0914.job@gmail.com>
2019-03-04 20:43:07 +0900

    第6回目の課題(再提出3/4)

---
+++ ColLesson01/src/com/company/Word.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Word {
+
+    public String word;
+    public String meaning;
+
+
+
+    public Word(String word, String meaning) {
+        this.word = word;
+        this.meaning = meaning;
+
+    }
+
+}
